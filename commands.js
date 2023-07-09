64bd79ce0a683da081e566f2d0c5a2b18a78891f
Vigil-X <139039236+Vigil-X@users.noreply.github.com>
2023-07-09 09:43:44 +0200

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,75 @@
+var youtube = "https://www.youtube.com";
+var twitter = "https://www.twitter.com//";
+var password = "test";
+var linkedin = "https://www.linkedin.com/in//";
+var instagram = "https://www.instagram.com/haris._a/";
+var github = "https://github.com//";
+var email = 'mailto:harisabd.ha@gmail.com';
+var deja_vu = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+
+about = [
+  "<br>",
+  "Just a little project of mine still under development",
+  "<br>"
+];
+
+social = [
+  "<br>",
+//  'youtube        <a href="' + youtube + '" target="_blank">youtube/' + "</a>",
+//  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/haris._a' + '</a>',
+//  'github         <a href="' + github + '" target="_blank">github/' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">admin</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "...",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          -About?',
+  '<span class="command">social</span>         -Display social networks',
+  '<span class="command">projects</span>       -View coding projects',
+  '<span class="command">deja-vu</span>        -You probably have already seen this',
+  '<span class="command">secret</span>         -Password required',
+  '<span class="command">history</span>        -View command history',
+  '<span class="command">help</span>           -List of commands',
+  '<span class="command">send nudes</span>     -(:',
+  '<span class="command">clear</span>          -Clear terminal',
+  '<span class="command">banner</span>         -Display the header',
+  '<span class="command">download ram</span>   -Download more ram for free',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Test (HA) Watch your step. All rights reserved.</span>',
+  "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠙⣻⣷⣶⣤⣀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⡿⠋⠀⠀⠀⠀⢹⣿⣿⡟⠉⠉⠉⢻⡿⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠰⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣿⣿⣇⠀⠀⠀⠈⠇⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠉⠛⠿⣷⣤⡤⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣶⣦⣤⣤⣀⣀⣀⡀⠉⠀⠀⠀⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀",
+"⠀⠀⠀⢀⣀⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠛⠿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀",
+"⠀⠀⣰⣿⣿⣿⣿⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣦⠀⠀",
+"⠀⠀⣿⣿⣿⠁⠀⠈⠙⢿⣿⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⠀⠀",
+"⠀⠀⢿⣿⣿⣆⠀⠀⠀⠀⠈⠛⠿⣿⣶⣦⡤⠴⠀⠀⠀⠀⠀⣸⣿⣿⣿⡿⠀⠀",
+"⠀⠀⠈⢿⣿⣿⣷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⡿⠀⠀",
+"⠀⠀⠀⠀⠙⢿⣿⣿⣿⣶⣦⣤⣀⣀⡀⠀⠀⠀⣀⣠⣴⣾⣿⣿⡿⠃⠀⠀⠀",
+"⠀⠀⠀⠀⠀⠀⠈⠙⠻⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+download_ram = [
+'<span class="color2"><a href="img/ram.png" download="proposed_file_name">Click Here To Download Ram For Free...</a></span>'
+];
